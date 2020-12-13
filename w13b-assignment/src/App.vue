e3af22a9593d3b94d8354abb0d07650786594c41
kevans054 <kjevans@telus.net>
2020-12-13 14:20:19 -0700

    original code without bonus

---
+++ w13b-assignment/src/App.vue
@@ -0,0 +1,25 @@
+  <template>
+  <div id="app">
+    <vue-tweets></vue-tweets> <!-- instantiate -->
+  </div>
+</template>
+<script>
+import VueTweets from './components/Tweets.vue' //import
+export default {
+  name: 'App',
+  components: {
+    VueTweets //register
+    }
+}
+</script>
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ w13b-assignment/src/components/Tweets.vue
@@ -0,0 +1,70 @@
+<template>
+<div>
+<button @click="toggleView">Toggle View</button>
+  <div id="tweets" :class="{ 'list-view': listview }">
+    <article class="tweet" v-for="(tweet, index) in tweets" v-bind:key="index">
+      <h4>{{ tweet.date }}</h4>
+      ,
+      <h6>{{ tweet.text }}</h6>
+      ,
+      <p>{{ tweet.author }}</p>
+    </article>
+  </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "vue-tweets",
+  data() {
+    return {
+      listview: false,
+      tweets: [
+        {
+          author: "Karen",
+          date: "December 13, 2020",
+          text: "Tweet, Tweet, Tweet",
+        },
+        {
+          author: "Karen",
+          date: "December 13, 2020",
+          text: "Tweeter, Tweeter, Tweeter",
+        },
+        {
+          author: "Karen",
+          date: "December 13, 2020",
+          text: "Twitter, Twitter, Twitter",
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleView() {
+      this.listview = !this.listview;
+      alert("toggleview");
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+#tweets {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  background-color: rgb(156, 157, 233);
+  margin-top: 10px;
+}
+#tweets.list-view {
+  grid-template-columns: 1fr;
+}
+h4 {
+  background-color: plum;
+  font-size: 30px;
+}
+h6 {
+  background: green;
+  font-size: 30px;
+}
+p {
+  background-color: cornflowerblue;
+  font-size: 30px;
+}
+</style>
