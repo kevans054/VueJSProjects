171b169128b391fdb4a1d3182ef633f7552f8d85
kevans054 <kjevans@telus.net>
2020-12-22 19:44:59 -0700

    updated version

--- src/components/JokeButton.vue
+++ src/components/JokeButton.vue
@@ -1,15 +1,25 @@
 <template>
     <div id="JokeButton">
         <button id="NewJokes" type="submit" @click="getJoke">New Joke</button>
-        
         <p>{{joke}}</p>
+        <normal-joke></normal-joke>
+        <loud-joke></loud-joke>
+        <snake-joke></snake-joke>
         
     </div>
 </template>
 
 <script>
+    import NormalJoke from './NormalJoke.vue';
+    import LoudJoke from './LoudJoke.vue';
+    import SnakeJoke from './SnakeJoke.vue';
     export default {
         name: "JokeButton",
+        components: {
+            NormalJoke ,
+            LoudJoke,
+            SnakeJoke,
+        },
         data() {
             return{
                 joke: ''
